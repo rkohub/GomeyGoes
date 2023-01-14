4fbcfc48b83ca28d428bb7643e8b48c7b0854151
ryanextra01 <51272897+rkohub@users.noreply.github.com>
2023-01-13 22:44:10 -0500

    Added Nice Spawns

--- My project copy/Assets/PlayerSpawner.cs
+++ My project copy/Assets/PlayerSpawner.cs
@@ -8,6 +8,7 @@ public class PlayerSpawner : MonoBehaviour{
     public float timeToSpawn;
     public float lastSpawned;
     public Vector3 spawnLocation;
+    public GameObject largeStudent;
 
     // Start is called before the first frame update
     void Start(){
@@ -23,8 +24,75 @@ public class PlayerSpawner : MonoBehaviour{
     }
 
     void spawn(){
-        int lane = Random.Range(0,3) - 1;
-        Instantiate(student, spawnLocation + new Vector3(2*lane,0,0), Quaternion.identity);
-        lastSpawned = 0;
+        int pattern = Random.Range(0,5);
+        print (pattern);
+        if(pattern == 0){
+            //1 Small
+            int lane = Random.Range(0,3) - 1;
+            spawnPlayer(student, lane);
+        }else if(pattern == 1){
+            //1 Large
+            int lane = Random.Range(0,3) - 1;
+            spawnPlayer(largeStudent, lane);
+        }else if(pattern == 2){
+            //2 Small
+            int lane = Random.Range(0,3) - 1; //Lane Not To Spawn
+            if(lane != 0){
+                spawnPlayer(student, 0);
+            }
+            if(lane != 1){
+                spawnPlayer(student, 1);
+            }
+            if(lane != -1){
+                spawnPlayer(student, -1);
+            }
+        }else if(pattern == 3){
+            //2 Big
+            int lane = Random.Range(0,3) - 1; //Lane Not To Spawn
+            if(lane != 0){
+                spawnPlayer(largeStudent, 0);
+            }
+            if(lane != 1){
+                spawnPlayer(largeStudent, 1);
+            }
+            if(lane != -1){
+                spawnPlayer(largeStudent, -1);
+            }
+        }else if(pattern == 4){
+            //random
+            int lane = Random.Range(0,3) - 1; //Lane Not To Spawn
+            if(lane != 0){
+                GameObject genStu = randomStudent();
+                spawnPlayer(genStu, 0);
+            }
+            if(lane != 1){
+                GameObject genStu = randomStudent();
+                spawnPlayer(genStu, 1);
+            }
+            if(lane != -1){
+                GameObject genStu = randomStudent();
+                spawnPlayer(genStu, -1);
+            }
+        }
+
+        lastSpawned = 0; //Random.Range(-1.0f,0.0f);
+
+        
+    }
+
+    void spawnPlayer(GameObject obj, int lane){
+        //-1,0,1 = lane
+        Instantiate(obj, spawnLocation + new Vector3(2*(lane),0,0), Quaternion.identity);
+    }
+
+    public GameObject randomStudent(){
+        int numStudents = 2;
+        int randStu = Random.Range(0, numStudents);
+        if(randStu == 0){
+            return student;
+        }else if(randStu == 1){
+            return largeStudent;
+        }
+        return null;
     }
 }
